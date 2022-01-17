2a5d83bea81d4e1989415f11114f22f8d501dad3
ussalbt <93818868+ussalbt@users.noreply.github.com>
2022-01-17 14:47:44 +0300

    Create 17.sql matematiksel işlem-.sql

---
+++ 17.sql matematiksel işlem-.sql
@@ -0,0 +1,17 @@
+select model_year
+from product.product
+group by model_year
+
+select distinct model_year 
+from product.product
+
+update product.product
+set model_year= 2022
+
+select *
+from product.product
+
+
+
+
+
